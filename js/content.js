7d5274bf15ebbd3e701c8a865b27edca1fb67a3b
Adam Pritchard <pritchard.adam@gmail.com>
2019-07-27 12:54:59 -0400

    Fix: Wasn't working with multiple checkboxes
    Such as occurs in the Threads view.

--- js/content.js
+++ js/content.js
@@ -15,30 +15,32 @@ function realClickListener(event) {
 
 window.addEventListener('load', (event) => {
 	setInterval(() => {
-		const channelSendCheckbox = document.querySelector('input[class$="broadcast_checkbox"]');
-		if (!channelSendCheckbox) {
-			// There's no "also send to channel" checkbox on the page.
-			return;
-		}
+		// In the "Threads" view, if there's also a thread open on the side, there may be
+		// multiple "Also send to channel" checkboxes, so we'll use querySelectorAll.
+		// Note that this selector is just reverse-engineered from looking at the DOM. It
+		// might be fragile.
+		const channelSendCheckboxes = document.querySelectorAll('input[class$="broadcast_checkbox"]');
 
-		if (channelSendCheckbox.checked) {
-			// Skip all the other checks and data accesses
-			return;
-		}
+		for (let i = 0; i < channelSendCheckboxes.length; i++) {
+			if (channelSendCheckboxes[i].checked) {
+				// Skip all the other checks and data accesses
+				continue;
+			}
 
-		if (channelSendCheckbox.dataset['__stopChecking']) {
-			// The user has interacted with the checkbox and we should no longer mess with it
-			return;
-		}
+			if (channelSendCheckboxes[i].dataset['__stopChecking']) {
+				// The user has interacted with the checkbox and we should no longer mess with it
+				continue;
+			}
 
-		// We need to use click() instead of checked=true because otherwise Slack just ignores it
-		channelSendCheckbox.click();
+			// We need to use click() instead of checked=true because otherwise Slack just ignores it
+			channelSendCheckboxes[i].click();
 
-		// Checking that the listener hasn't already been added is redundant, since
-		// addEventListener won't add the same listener twice, but...
-		if (!channelSendCheckbox.dataset['__realClickListenerAdded']) {
-			channelSendCheckbox.addEventListener('click', realClickListener);
-			channelSendCheckbox.dataset['__realClickListenerAdded'] = true;
+			// Checking that the listener hasn't already been added is redundant, since
+			// addEventListener won't add the same listener twice, but...
+			if (!channelSendCheckboxes[i].dataset['__realClickListenerAdded']) {
+				channelSendCheckboxes[i].addEventListener('click', realClickListener);
+				channelSendCheckboxes[i].dataset['__realClickListenerAdded'] = true;
+			}
 		}
 	}, 500);
 });
